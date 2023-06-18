864aeda1a1bb8373ad3549717a51efc2f52ebd41
Avery <74130268+velara@users.noreply.github.com>
2023-06-18 08:24:55 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ export const MODULE_ID = "lite-ruler";
 Hooks.once("init", () => {
 	//Wait until the game is initialized, then register the settings created previously.
   registerKeybindings();
+  game.settings.register("lite-ruler", "liteRulerDisabled", {
+		name: "Disable Lite Ruler",
+		hint: "Disables Lite Ruler's Functionality",
+		scope: "world",
+		config: true,
+		type: Boolean,
+		default: false
+});
 });
 
 Hooks.once("libWrapper.Ready", async function() {
@@ -11,40 +19,39 @@ Hooks.once("libWrapper.Ready", async function() {
 });
 
 function registerRuler() {
-	libWrapper.register(MODULE_ID, "Token.prototype._onDragLeftStart", beginLiteRuler, libWrapper.OVERRIDE);
+	libWrapper.register(MODULE_ID, "Token.prototype._onDragLeftStart", beginLiteRuler, libWrapper.WRAPPER);
 	libWrapper.register(MODULE_ID, "Token.prototype._onDragLeftMove", onTokenLeftDragMove, libWrapper.WRAPPER);
 	libWrapper.register(MODULE_ID, "Token.prototype._onDragLeftDrop", onTokenDragLeftDrop, libWrapper.WRAPPER);
 	libWrapper.register(MODULE_ID, "Token.prototype._onDragLeftCancel", onTokenDragLeftCancel, libWrapper.WRAPPER);
 }
 
-function beginLiteRuler(event) {
-	return canvas.controls.ruler._onDragStart(event);//};
+function beginLiteRuler(wrapper, event) {
+if(game.settings.get("lite-ruler", "liteRulerDisabled")){return wrapper(event)}
+  else canvas.controls.ruler._onDragStart(event);
+  return wrapper(event);
+
 }
 
 function onTokenLeftDragMove(wrapper, event) {
-		canvas.controls.ruler._onMouseMove(event)
-		return wrapper(event);
+  if(game.settings.get("lite-ruler", "liteRulerDisabled")){return wrapper(event)}
+    else canvas.controls.ruler._onMouseMove(event)
+	return wrapper(event);
 }
 
 function onTokenDragLeftDrop(wrapper, event) {
-	const tokenStart = canvas.grid.grid.getGridPositionFromPixels(event.data.origin.x, event.data.origin.y);
-	const tokenEnd = canvas.grid.grid.getGridPositionFromPixels(event.data.destination.x, event.data.destination.y)
-	if (tokenStart.toString() !== tokenEnd.toString() && canvas.controls.ruler._state > 0) {
-		const selectedTokens = canvas.tokens.placeables.filter(token => token._controlled);
-		canvas.controls.ruler.moveToken(canvas.controls.ruler, selectedTokens)
-	}
+  if(game.settings.get("lite-ruler", "liteRulerDisabled")){return wrapper(event)}
+    else canvas.controls.ruler.moveToken(event)
 	return wrapper(event);
 }
 
 function onTokenDragLeftCancel(wrapper, event) {
-	const ruler = canvas.controls.ruler;
+  if(game.settings.get("lite-ruler", "liteRulerDisabled")){return wrapper(event)}
+    else {const ruler = canvas.controls.ruler;
 	if ( (ruler._state === 2) && (ruler.waypoints.length > 1) ) {
-		event.preventDefault();
-		const mousePosition = canvas.app.renderer.plugins.interaction.mouse.getLocalPosition(canvas.tokens)
-	  ruler._removeWaypoint({x: mousePosition.x, y: mousePosition.y});
-	  game.user.broadcastActivity({ruler: canvas.controls.ruler});
+		ruler._removeWaypoint()
+    return wrapper(event);
  }
 	else ruler._endMeasurement();
-
-	return wrapper(event);
+  return wrapper(event);
 	}
+}
